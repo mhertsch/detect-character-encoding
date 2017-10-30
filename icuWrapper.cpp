30424c821b7e6f859afebbd317f922681667b280
Michael Hertsch <mhertsch@t-online.de>
2017-10-30 10:27:26 +0100

    New: Added placeholder for DetectAllCharacterEncodings

--- icuWrapper.cpp
+++ icuWrapper.cpp
@@ -8,6 +8,10 @@ NAN_METHOD(DetectCharacterEncoding) {
 
 	v8::Local<v8::Object> inputBuffer = info[0]->ToObject();
 
+    if (!node::Buffer::HasInstance(inputBuffer)) {
+      return Nan::ThrowTypeError("Buffer instance expected!");
+    }
+
 	UCharsetDetector *charsetDetector;
 	const UCharsetMatch *charsetMatch;
 	UErrorCode errorCode = U_ZERO_ERROR;
@@ -64,9 +68,74 @@ NAN_METHOD(DetectCharacterEncoding) {
 	ucsdet_close(charsetDetector);
 }
 
-void Init(v8::Local<v8::Object> exports) {
-	exports->Set(Nan::New<v8::String>("detectCharacterEncoding").ToLocalChecked(),
-		Nan::New<v8::FunctionTemplate>(DetectCharacterEncoding)->GetFunction());
+NAN_METHOD(DetectAllCharacterEncodings) {
+	Nan::HandleScope scope;
+
+	v8::Local<v8::Object> inputBuffer = info[0]->ToObject();
+
+    if (!node::Buffer::HasInstance(inputBuffer)) {
+      return Nan::ThrowTypeError("Buffer instance expected!");
+    }
+
+	UCharsetDetector *charsetDetector;
+	const UCharsetMatch *charsetMatch;
+	UErrorCode errorCode = U_ZERO_ERROR;
+
+	charsetDetector = ucsdet_open(&errorCode);
+
+	if(U_FAILURE(errorCode)) {
+		Nan::ThrowError("Failed to open ICU charset detector.");
+		return;
+	}
+
+	ucsdet_setText(
+		charsetDetector,
+		node::Buffer::Data(inputBuffer),
+		node::Buffer::Length(inputBuffer),
+		&errorCode
+	);
+
+	if(U_FAILURE(errorCode)) {
+		Nan::ThrowError("Failed to set ICU charset detector’s text.");
+		ucsdet_close(charsetDetector);
+		return;
+	}
+
+	charsetMatch = ucsdet_detect(charsetDetector, &errorCode);
+
+	if(U_FAILURE(errorCode)) {
+		Nan::ThrowError("Failed to detect charset.");
+		ucsdet_close(charsetDetector);
+		return;
+	}
+
+	const char *charsetName = ucsdet_getName(charsetMatch, &errorCode);
+
+	if(U_FAILURE(errorCode)) {
+		Nan::ThrowError("Failed to get name from charset match.");
+		ucsdet_close(charsetDetector);
+		return;
+	}
+
+	int32_t confidence = ucsdet_getConfidence(charsetMatch, &errorCode);
+
+	if(U_FAILURE(errorCode)) {
+		Nan::ThrowError("Failed to get confidence from charset match.");
+		ucsdet_close(charsetDetector);
+		return;
+	}
+
+	v8::Local<v8::Object> obj = Nan::New<v8::Object>();
+	obj->Set(Nan::New<v8::String>("encoding").ToLocalChecked(), Nan::New<v8::String>(charsetName).ToLocalChecked());
+	obj->Set(Nan::New<v8::String>("confidence").ToLocalChecked(), Nan::New<v8::Number>(confidence));
+
+	info.GetReturnValue().Set(obj);
+	ucsdet_close(charsetDetector);
+}
+
+NAN_MODULE_INIT(Init) {
+    NAN_EXPORT(target, DetectCharacterEncoding);
+    NAN_EXPORT(target, DetectAllCharacterEncodings);
 }
 
 NODE_MODULE(icuWrapper, Init);
--- test/index.js
+++ test/index.js
@@ -4,22 +4,36 @@ const assert = require('assert');
 const fs = require('fs');
 const path = require('path');
 
-const detectCharacterEncoding = require('..');
+const icuWrapper = require('bindings')('icuWrapper');
 
-function getFixture(fixture) {
-	return fs.readFileSync(path.join(__dirname, 'fixtures', fixture));
-}
+describe('DetectCharacterEncoding', () => {
+	const detectCharacterEncoding = icuWrapper.DetectCharacterEncoding;
 
-it('should return the encoding', () => {
-	assert.equal(detectCharacterEncoding(getFixture('utf-8.txt')).encoding, 'UTF-8');
+	function getFixture(fixture) {
+		return fs.readFileSync(path.join(__dirname, 'fixtures', fixture));
+	}
+
+	it('should return the encoding', () => {
+		assert.equal(detectCharacterEncoding(getFixture('utf-8.txt')).encoding, 'UTF-8');
+	});
+
+	it('should return a confidence value', () => {
+		assert(typeof detectCharacterEncoding(getFixture('utf-8.txt')).confidence === 'number');
+	});
+
+	it('should throw a TypeError if argument is not a buffer', () => {
+		assert.throws(() => {
+			detectCharacterEncoding('string');
+		}, TypeError);
+	});
 });
 
-it('should return a confidence value', () => {
-	assert(typeof detectCharacterEncoding(getFixture('utf-8.txt')).confidence === 'number');
-});
+describe('DetectCharacterEncoding', () => {
+	const detectAllCharacterEncodings = icuWrapper.DetectAllCharacterEncodings;
 
-it('should throw a TypeError if argument is not a buffer', () => {
-	assert.throws(() => {
-		detectCharacterEncoding('string');
-	}, TypeError);
+	it('should throw a TypeError if argument is not a buffer', () => {
+		assert.throws(() => {
+			detectAllCharacterEncodings('string');
+		}, TypeError);
+	});
 });
